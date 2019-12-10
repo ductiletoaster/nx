e8b491d43dfe19b31e3b5dc3ff46e06617cf3b37
Jason Jean <jasonjean1993@gmail.com>
2019-12-10 09:50:12 -0500

    Revert "Revert "feat(core): update to next version of angular deps""
    This reverts commit f816fc424d4b0d850e0a49d0ed8e4a30ddca308c.

--- packages/workspace/src/core/shared-interfaces.ts
+++ packages/workspace/src/core/shared-interfaces.ts
@@ -15,7 +15,7 @@ export interface NxJson<T = '*' | string[]> {
   tasksRunnerOptions?: {
     [tasksRunnerName: string]: {
       runner: string;
-      options?: unknown;
+      options?: object;
     };
   };
 }
